85d3823f8403f6a4590465ec3edbbb4642e2af0f
Michael Bodnar <1059765+BodnarMichael@users.noreply.github.com>
2020-03-22 12:50:18 +0100

    improved clarity of filenames in script and output

--- coding/code/M1_4_1_word_sentence_vectorisation_1d.py
+++ coding/code/M1_4_1_word_sentence_vectorisation_1d.py
@@ -79,8 +79,8 @@ input_file = "https://raw.githubusercontent.com/MaximilianKupi/nlp-project/maste
 path = "exchange_base/"
 stage = "train"
 # input_file = path + stage + "_set.csv"
-output_file_name_vectorized = path + stage +  "_vectorized.pt"
-output_file_name_labels = path + stage +  "_labels.pt"
+output_file_name_vectorized = path + stage +  "_vectorized_1d.pt"
+output_file_name_labels = path + stage +  "_labels_1d.pt"
 
 # loading data
 data = pd.read_csv(input_file)
