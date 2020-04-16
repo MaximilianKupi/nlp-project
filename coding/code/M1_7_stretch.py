6f7b49480050291a8eb1b1dfccb36335e8f911da
Michael Bodnar <1059765+BodnarMichael@users.noreply.github.com>
2020-04-16 19:26:08 +0200

    added stretching function

---
+++ coding/code/M1_7_stretch.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def stretch(vector,n,plot=False):
+    """Stretches Vectors to desired length by interpolating values
+
+
+    Example
+    stretch([0,0,0,0,20,0,0,1],15)
+
+    with plotting
+    stretch([0,0,0,0,20,0,0,1],15,True)
+
+    Args:
+        vector (array(float)): The input vector that should be stretched
+        n (int): The length of the output vector after stretching
+
+    Returns:
+        np.array: stretched vector
+
+    """
+
+    if(n<len(vector)):
+        raise ValueError("Shrinking not allowed")
+    # Converts input to numpy array
+    oldY = np.array(vector)
+    # Treates input as y-values of a function on x-values created here
+    oldX = np.linspace(0,len(vector)-1,len(vector))
+
+    # Determine non-zero values that we have to keep
+    # If we deviate from these points in the interpolation
+    # then the result will shrink the dictionary evaluation number
+    keep = np.where(oldY != 0)[0]
+
+    # Create new vector with desired length
+    newX = np.linspace(0,len(vector)-1,n)
+
+    # Make sure that the nonzero values are kept as is after interpolation
+    # by replacing the closest values with the ones we saved in keep
+    for value in keep:
+        newX[np.abs(np.array(newX) - value).argmin()] = value
+
+    # interpolate to new 
+    newY = np.interp(newX,oldX,oldY)
+
+    if(plot):
+        plt.plot(oldX,oldY)
+        plt.plot(newX,newY)
+    return newY
